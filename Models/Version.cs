212b6666c976a150a305bf46ef2620a5cf731dee
“dasktelor” <dasktelor@mail.ru>
2022-12-15 00:29:28 +0700

    extended VersionController

---
+++ Models/Version.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab_WebAPI.Models
+{
+    public class Version
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+}
