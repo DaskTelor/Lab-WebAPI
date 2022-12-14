4d5dc1ee1e9c3e3fb205170fcf14fd75342dea55
“dasktelor” <dasktelor@mail.ru>
2022-12-15 00:27:40 +0700

    initial commit

---
+++ Controllers/PersonalComputerController.cs
@@ -0,0 +1,50 @@
+﻿using Lab_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonalComputerController : ControllerBase
+    {
+        private static List<PersonalComputerData> _memCache = new List<PersonalComputerData>();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PersonalComputerData>> Get()
+        {
+            return _memCache;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PersonalComputerData> Get(int id)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            return _memCache[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] PersonalComputerData value)
+        {
+            _memCache.Add(value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] PersonalComputerData value)
+        {
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            _memCache[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+            if (_memCache.Count <= id) throw new IndexOutOfRangeException("Нет такого у нас");
+
+            _memCache.RemoveAt(id);
+        }
+    }
+
+}
---
+++ Models/PersonalComputerData.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Lab_WebAPI.Models
+{
+    public class ProcessorData
+    {
+        public string? Producer { get; set; }//производитель
+        public string? Name { get; set; }//наименование
+        public int BaseFrequency { get; set; }//базовая частота MHz
+        public int AutoaccelerationFrequency { get; set; }//частота авторазгона MHz
+        public int NumberOfCores { get; set; }//количество ядер
+    }
+    public class VideoCardData
+    {
+        public string? Producer { get; set; }//производитель
+        public string? Name { get; set; }//наименование
+        public int VRAM { get; set; }//видеопамять MB
+    }
+    public class PersonalComputerData
+    {
+        public ProcessorData? CPU { get; set; }
+        public VideoCardData? GPU { get; set; }
+    }
+}
